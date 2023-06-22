a3eae2915e1ae33cbe2ad6512ae6bc0604a2363c
krishnasatvik <109426680+krishnasatvik@users.noreply.github.com>
2023-06-22 15:45:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
-resource "google_storage_bucket" "default" {
-  name     = "tf-bucket2"
+resource "google_compute_network" "vpc1" {
+  name                    = "my-custom-network-1"
+  auto_create_subnetworks = "false"
+
+}
+
+resource "google_compute_subnetwork" "my-custom-subnet1" {
+  name          = "my-custom-subnet-1"
+  ip_cidr_range = "10.255.196.0/24"
+  network       = google_compute_network.vpc1.name
+  region        = "us-east1"
+}
+
   
   service_account {
     scopes = ["cloud-platform"]
